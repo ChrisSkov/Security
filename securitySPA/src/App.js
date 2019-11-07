ac862ca37ded77572b9c72b12f7189cd5662e73f
Chris <Stofferdyr@gmail.com>
2019-11-07 10:02:06 +0100

    redo spa

--- securitySPA/src/App.js
+++ securitySPA/src/App.js
@@ -1,81 +1,70 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 import facade from "./apiFacade";
-class LogIn extends Component
-{
-  constructor(props)
-  {
+class LogIn extends Component {
+  constructor(props) {
     super(props);
-    this.state = { username: "", password: "" }
+    this.state = { username: "", password: "" };
   }
-  login = (evt) =>
-  {
+  login = evt => {
     evt.preventDefault();
     this.props.login(this.state.username, this.state.password);
-  }
-  onChange = (evt) =>
-  {
-    this.setState({ [evt.target.id]: evt.target.value })
-  }
-  render()
-  {
+  };
+  onChange = evt => {
+    this.setState({ [evt.target.id]: evt.target.value });
+  };
+  render() {
     return (
       <div>
         <h2>Login</h2>
-        <form onSubmit={this.login} onChange={this.onChange} >
+        <form onSubmit={this.login} onChange={this.onChange}>
           <input placeholder="User Name" id="username" />
           <input placeholder="Password" id="password" />
           <button>Login</button>
         </form>
       </div>
-    )
+    );
   }
 }
-class LoggedIn extends Component
-{
-  constructor(props)
-  {
+class LoggedIn extends Component {
+  constructor(props) {
     super(props);
     this.state = { dataFromServer: "Fetching!!" };
   }
-  componentDidMount() { facade.fetchData().then(res => this.setState({ dataFromServer: res })); }
-  render()
-  {
+  componentDidMount() {}
+  render() {
     return (
       <div>
         <h2>Data Received from server</h2>
         <h3>{this.state.dataFromServer}</h3>
       </div>
-    )
+    );
   }
 }
-class App extends Component
-{
-  constructor(props)
-  {
+class App extends Component {
+  constructor(props) {
     super(props);
-    this.state = { loggedIn: false }
+    this.state = { loggedIn: false };
   }
-  logout = () =>
-  {
+  logout = () => {
     facade.logout();
     this.setState({ loggedIn: false });
-  } //TODO
-  login = (user, pass) =>
-  {
-    facade.login(user, pass)
-      .then(res => this.setState({ loggedIn: true }));
-  } //TODO
-  render()
-  {
+  };
+  login = (user, pass) => {
+    facade.login(user, pass).then(res => this.setState({ loggedIn: true }));
+  };
+  render() {
     return (
       <div>
-        {!this.state.loggedIn ? (<LogIn login={this.login} />) :
-          (<div>
+        {!this.state.loggedIn ? (
+          <LogIn login={this.login} />
+        ) : (
+          <div>
             <LoggedIn />
             <button onClick={this.logout}>Logout</button>
-          </div>)}
+          </div>
+        )}
       </div>
-    )
+    );
   }
 }
 export default App;
